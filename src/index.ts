eb7cf5f36fee956b957a5db974f796419b8b6ee0
boolid <hello@cjjeon.com>
2022-11-22 05:22:20 -0800

    Adding monster

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Canvas from "./canvas";
 import Layout from "./components/layout";
+import Monster from "./components/monster";
 import Player from "./components/player";
 
 
 const player = new Player({})
+const monster = new Monster()
 const layout = new Layout()
-const canvas = new Canvas([layout, player]);
+const canvas = new Canvas([layout, monster, player]);
 canvas.start();
