e728fd6f5f0626f061713acd28c16ed9bf94f0df
boolid <hello@cjjeon.com>
2022-11-29 03:20:21 -0800

    Adding simple websocket and express to the server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import * as http from "http";
+import Websocket from 'ws';
 
-const index = express();
+const app = express();
+const server = http.createServer(app)
 const port = 3000;
 
-index.listen(port, () => {
+app.listen(port, () => {
     console.log(`Timezones by location application is running on port ${port}.`);
 });
+
+const wsServer = new Websocket.Server({server})
+wsServer.on('connection', (ws) => {
+
+    ws.on('message', (message: string) => {
+        console.log('received: ', message);
+        ws.send(`Hi!`)
+    })
+})
